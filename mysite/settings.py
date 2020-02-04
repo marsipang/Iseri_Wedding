9a0f76f0609da2e380f918a69fb806daedfa52c5
Marissa Pang <pangie490@gmail.com>
2020-02-03 15:03:17 -1000

    putting polls back in for now

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com',]
 
 INSTALLED_APPS = [
     'guests.apps.GuestsConfig',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
